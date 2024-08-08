2754778780bb3792ce851a706e51f27a738e973f
web-dev-with-Rahul <rishumalhotra705@gmail.com>
2024-08-08 18:30:48 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -13,4 +13,14 @@ app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))  //image save or folder so public folder for assests 
 app.use(cookieParser())    //cookie access from server to get 
 
+//routes import 
+import userRouter from './routes/user.routes.js'
+
+//routes declaration
+app.use("/api/v1/users",userRouter)
+
+// http://localhost:8000/api/v1/users/register
+
+
+
 export {app}
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({ 
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key:process.env.CLOUDINARY_CLOUD_KEY, 
+    api_secret: process.env.CLOUDINARY_CLOUD_SECRET 
+});
+
+const  uploadOnCloudinary=async(localFilePath)=>{
+    try{
+        if(!localFilePath) return null
+        //upload dile on cloudinary
+        const response=await cloudinary.uploader.upload(localFilePath,{
+            resource_type:"auto"
+        })
+        //file has been uploaded successfully
+        console.log("File uploaded successfully on cloudinary",response.url);
+        return response;
+
+    } catch(error){
+        fs.unlinkSync(localFilePath) // remoce local save temporary file upload operation got failed
+    }
+}
+
+
+export {uploadOnCloudinary}
